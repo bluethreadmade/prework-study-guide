9ef3924c7ef2d7a74da35a758fbfa21dec6727ef
Megan McNeill <mcneim2@gmail.com>
2024-05-12 21:53:31 -0400

    adding for loop and array of topics

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for(let x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+    
+    /*{
+ if (x === 0) {
+  console.log("Let's study HTML!");
+} else if (x === 1) {
+  console.log("Let's study CSS!");
+} else if (x === 2) {
+  console.log("Let's study Git!");
+} else if (x === 3) {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
+} 
+const shapes = ["triangle", "square", "pentagon", "circle"];
+for(let x = 0; x < shapes.length; x++) {
+console.log(shapes[x]);
+}
+*/
